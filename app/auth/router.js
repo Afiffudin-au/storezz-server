0a2ae90b75fe761d85b4d4de55b9f22affcd4e38
Afiffudin-au <afiffudin077@gmail.com>
2022-05-17 23:37:50 +0700

    first commit

---
+++ app/auth/router.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var router = express.Router()
+const multer = require('multer')
+const os = require('os')
+const { signup, signin } = require('./controller')
+router.post('/signup', multer({ dest: os.tmpdir() }).single('image'), signup)
+router.post('/signin', multer({ dest: os.tmpdir() }).single('image'), signin)
+module.exports = router
---
+++ app/transaction/controller.js
@@ -0,0 +1,50 @@
+const Transaction = require('./model')
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash('alertMessage')
+      const alertStatus = req.flash('alertStatus')
+      const alert = { message: alertMessage, status: alertStatus }
+      const transaction = await Transaction.find().populate('player')
+      res.render('admin/transaction/view_transaction', {
+        transaction,
+        alert,
+        name: req.session.user.name,
+        title: 'Halaman Transaction',
+      })
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/transaction')
+    }
+  },
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params
+      const { status } = req.query
+      await Transaction.findByIdAndUpdate({ _id: id }, { status })
+      req.flash('alertMessage', `Berhasil ubah status`)
+      req.flash('alertStatus', 'success')
+      res.redirect('/transaction')
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/transaction')
+    }
+  },
+  viewDetailTransaction: async (req, res) => {
+    try {
+      const { id } = req.params
+      const detailTransactions = await Transaction.findOne({ _id: id })
+      res.render('admin/transaction/detail_transaction', {
+        detailTransactions,
+        name: req.session.user.name,
+        title: 'Halaman detail transaction',
+      })
+    } catch (err) {
+      req.flash('alertMessage', `${err.message}`)
+      req.flash('alertStatus', 'danger')
+      res.redirect('/transaction')
+    }
+  },
+}
